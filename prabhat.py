85f0373b7f007223ca7515440eae09566634b3e6
vivekvermaiet <vivekvermaiet@gmail.com>
2021-09-06 17:56:32 +0530

    Update prabhat.py

--- prabhat.py
+++ prabhat.py
@@ -5,3 +5,8 @@ sum = 0
 for i in range(1, 10):
     sum = sum + i
 print("Sum of first 10 numbers :", sum)
+# performing mul of first 10 numbers
+mul = 1
+for i in range(1, 10):
+    mul = mul + i
+print("Mul of first 10 numbers :", mul)
